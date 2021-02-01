990dc260a4ce15226672e34c2576e0a014d5ef8f
safwan <safwan.mansuri@peak.ai>
2021-02-01 20:34:59 +0530

    addd seeders

---
+++ seeders/20210201133237-addMusic.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+      await queryInterface.bulkInsert('music', [{
+        name: 'kaise hua',
+        genre: 'romantic',
+        singer: 'arijit',
+        writer: 'safwan',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
