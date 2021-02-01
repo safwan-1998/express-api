64eaafe23b6ccac1610a4ccd6547c066587e6bc5
safwan <safwan.mansuri@peak.ai>
2021-02-02 01:23:27 +0530

    did some changes

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN npm install
 
 COPY . .
 
+ENV NODE_ENV=production
+
 EXPOSE 5000
 
 CMD ["npm", "start"]
--- config/config.js
+++ config/config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  development: {
+  production: {
     host: "postgres-db",
     username: "postgres",
     password: "Safwanman@2",
     database: "postgres",
     dialect: 'postgres',
   },
+  development: {
+    host: "localhost",
+    username: "postgres",
+    password: "Safwanman@2",
+    database: "peak",
+    dialect: 'postgres',
+  }
 };
