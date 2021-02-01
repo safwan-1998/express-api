596467c7b9173974ceeb41fce02710ac5ec3e61f
safwan <safwan.mansuri@peak.ai>
2021-02-01 23:20:48 +0530

    changes

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    host: "postgres-db",
+    username: "postgres",
+    password: "Safwanman@2",
+    database: "postgres",
+    dialect: 'postgres',
+  },
+};
